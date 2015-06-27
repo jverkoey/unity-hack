48bc174e0f43c60feb00bb4bfe68f48e38d28709
Jeff Verkoeyen <jverkoey@gmail.com>
2015-06-27 18:50:26 -0400

    Random level generation.

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -24,14 +24,9 @@ public class BoardManager : MonoBehaviour
 	
 	public int columns = 8;                                         //Number of columns in our game board.
 	public int rows = 8;                                            //Number of rows in our game board.
-	public Count wallCount = new Count (5, 9);                      //Lower and upper limit for our random number of walls per level.
-	public Count foodCount = new Count (1, 5);                      //Lower and upper limit for our random number of food items per level.
-	public GameObject exit;                                         //Prefab to spawn for exit.
+	public Count doodadCount = new Count (10, 50);  
 	public GameObject[] floorTiles;                                 //Array of floor prefabs.
-	public GameObject[] wallTiles;                                  //Array of wall prefabs.
-	public GameObject[] foodTiles;                                  //Array of food prefabs.
-	public GameObject[] enemyTiles;                                 //Array of enemy prefabs.
-	public GameObject[] outerWallTiles;                             //Array of outer tile prefabs.
+	public GameObject[] doodadTiles;
 	
 	private Transform boardHolder;                                  //A variable to store a reference to the transform of our Board object.
 	private List <Vector3> gridPositions = new List <Vector3> ();   //A list of possible locations to place tiles.
@@ -50,7 +45,7 @@ public class BoardManager : MonoBehaviour
 			for(int y = 1; y < rows-1; y++)
 			{
 				//At each index add a new Vector3 to our list with the x and y coordinates of that position.
-				gridPositions.Add (new Vector3(x, y, 0f));
+				gridPositions.Add (new Vector3(x, y, -1f));
 			}
 		}
 	}
@@ -70,11 +65,7 @@ public class BoardManager : MonoBehaviour
 			{
 				//Choose a random tile from our array of floor tile prefabs and prepare to instantiate it.
 				GameObject toInstantiate = floorTiles[Random.Range (0,floorTiles.Length)];
-				
-				//Check if we current position is at board edge, if so choose a random outer wall prefab from our array of outer wall tiles.
-				if(x == -1 || x == columns || y == -1 || y == rows)
-					toInstantiate = outerWallTiles [Random.Range (0, outerWallTiles.Length)];
-				
+
 				//Instantiate the GameObject instance using the prefab chosen for toInstantiate at the Vector3 corresponding to current grid position in loop, cast it to GameObject.
 				GameObject instance =
 					Instantiate (toInstantiate, new Vector3 (x, y, 0f), Quaternion.identity) as GameObject;
@@ -125,27 +116,15 @@ public class BoardManager : MonoBehaviour
 	
 	
 	//SetupScene initializes our level and calls the previous functions to lay out the game board
-	public void SetupScene (int level)
+	public void SetupScene ()
 	{
 		//Creates the outer walls and floor.
 		BoardSetup ();
 		
 		//Reset our list of gridpositions.
 		InitialiseList ();
-		
-		//Instantiate a random number of wall tiles based on minimum and maximum, at randomized positions.
-		LayoutObjectAtRandom (wallTiles, wallCount.minimum, wallCount.maximum);
-		
-		//Instantiate a random number of food tiles based on minimum and maximum, at randomized positions.
-		LayoutObjectAtRandom (foodTiles, foodCount.minimum, foodCount.maximum);
-		
-		//Determine number of enemies based on current level number, based on a logarithmic progression
-		int enemyCount = (int)Mathf.Log(level, 2f);
-		
-		//Instantiate a random number of enemies based on minimum and maximum, at randomized positions.
-		LayoutObjectAtRandom (enemyTiles, enemyCount, enemyCount);
-		
-		//Instantiate the exit tile in the upper right hand corner of our game board
-		Instantiate (exit, new Vector3 (columns - 1, rows - 1, 0f), Quaternion.identity);
+
+		LayoutObjectAtRandom (doodadTiles, doodadCount.minimum, doodadCount.maximum);
+
 	}
 }
---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetComponent<UnityEngine.Camera>().orthographicSize = (Screen.height / 32f / 2.0f); // 100f is the PixelPerUnit that you have set on your sprite. Default is 100.	
+	}
+}
