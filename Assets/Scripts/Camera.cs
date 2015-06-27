3284e4bfdfb10d877866a5bbdba5681430339d0e
Jeff Verkoeyen <jverkoey@gmail.com>
2015-06-27 19:01:32 -0400

    Mouse to move around.

--- Assets/Scripts/Camera.cs
+++ Assets/Scripts/Camera.cs
@@ -2,15 +2,43 @@
 using System.Collections;
 
 public class Camera : MonoBehaviour {
+	
+	public float Boundary = 50; // distance from edge scrolling starts
+	public float speed = 15f;
+
+	private int theScreenWidth;
+	private int theScreenHeight;
 
 	// Use this for initialization
 	void Start () {
 		GetComponent<UnityEngine.Camera>().orthographicSize = (Screen.height / 32f / 2.0f); // 100f is the PixelPerUnit that you have set on your sprite. Default is 100.	
+		
+		theScreenWidth = Screen.width;
+		theScreenHeight = Screen.height;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		float speed = 15f;
+		
+		if (Input.mousePosition.x > theScreenWidth - Boundary)
+		{
+			transform.Translate(new Vector3(speed * Time.deltaTime,0,0));
+		}
+		
+		if (Input.mousePosition.x < 0 + Boundary)
+		{
+			transform.Translate(new Vector3(-speed * Time.deltaTime,0,0));
+		}
+		
+		if (Input.mousePosition.y > theScreenHeight - Boundary)
+		{
+			transform.Translate(new Vector3(0,speed * Time.deltaTime,0));
+		}
+		
+		if (Input.mousePosition.y < 0 + Boundary)
+		{
+			transform.Translate(new Vector3(0,-speed * Time.deltaTime,0));
+		}
 		if(Input.GetKey(KeyCode.RightArrow))
 		{
 			transform.Translate(new Vector3(speed * Time.deltaTime,0,0));
